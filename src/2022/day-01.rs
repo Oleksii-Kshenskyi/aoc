e0d3139eb794ebc8c92221f9fb5c5d7c40612c72
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2024-02-17 20:37:39 +0200

    Basic infrastructure for easier AoC input access established.

--- src/2022/day-01.rs
+++ src/2022/day-01.rs
@@ -1,7 +1,6 @@
 use anyhow::Result;
 
 fn main() -> Result<()> {
-    aoc::from_lib();
     println!("from 2022, day 01!");
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,45 @@
-pub fn from_lib() {
-    println!("from lib!");
+use std::fs;
+
+fn get_file_lines(filename: &str) -> Option<Vec<String>> {
+    match fs::read_to_string(format!("../inputs/{}", filename)) {
+        Ok(s) => Some(s.lines().map(String::from).collect()),
+        Err(_) => None,
+    }
+}
+
+pub struct Inputs {
+    pub sample: Option<Vec<String>>,
+    pub input: Option<Vec<String>>,
+}
+
+impl Inputs {
+    pub fn new(year: &str, day: &str) -> Self {
+        let sample_filename = format!("{}-day-{}-sample.txt", year, day);
+        let input_filename = format!("{}-day-{}-input.txt", year, day);
+        let sample = get_file_lines(&sample_filename);
+        let input = get_file_lines(&input_filename);
+
+        Self { sample, input }
+    }
+}
+
+pub struct DayResults {
+    pub part1: String,
+    pub part2: String,
+}
+
+impl DayResults {
+    pub fn new(one: String, two: String) -> DayResults {
+        Self {
+            part1: one,
+            part2: two,
+        }
+    }
+
+    pub fn print(&self, prompt: &str) {
+        println!(
+            "{}\n\tPart 1: {}.\n\tPart 2: {}.",
+            prompt, self.part1, self.part2
+        )
+    }
 }
