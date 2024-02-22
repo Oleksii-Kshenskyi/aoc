a8eed6f9504b74563ce980d66814c236279f0e84
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2024-02-22 21:28:52 +0200

    2023, Day 04, skeleton.

---
+++ src/2023/day-04.rs
@@ -0,0 +1,44 @@
+const YEAR: &'static str = "2023";
+const DAY: &'static str = "04";
+
+struct Card {}
+
+fn part1(_cards: &Vec<Card>) -> String {
+    "1".to_string()
+}
+
+fn part2(_cards: &Vec<Card>) -> String {
+    "2".to_string()
+}
+
+fn lines_to_cards(_lines: &Vec<String>) -> Vec<Card> {
+    vec![]
+}
+
+fn main() {
+    let inputs = aoc::Inputs::new(YEAR, DAY);
+
+    let sample_res = match &inputs.sample {
+        Some(input) => {
+            let cards = lines_to_cards(input);
+            aoc::DayResults::new(part1(&cards), part2(&cards))
+        }
+        None => aoc::DayResults::new(
+            "<SAMPLE INPUT UNAVAILABLE>".to_string(),
+            "<SAMPLE INPUT UNAVAILABLE>".to_string(),
+        ),
+    };
+    sample_res.print("=> Sample Results:");
+
+    let input_res = match &inputs.input {
+        Some(input) => {
+            let cards = lines_to_cards(input);
+            aoc::DayResults::new(part1(&cards), part2(&cards))
+        }
+        None => aoc::DayResults::new(
+            "<ACTUAL INPUT UNAVAILABLE>".to_string(),
+            "<ACTUAL INPUT UNAVAILABLE>".to_string(),
+        ),
+    };
+    input_res.print("=> Actual Input Results:");
+}
